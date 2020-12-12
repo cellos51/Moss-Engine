5dddbebf50bc70769d23060eb6541be565097877
Gordie Schiebel <gordieschiebel@gmail.com>
2020-12-12 17:52:00 -0500

    fixed a fix of a minor physics bug
    its either it sticks to walls or it jitters. i hope i fixed it this time

--- src/player.cpp
+++ src/player.cpp
@@ -76,7 +76,7 @@ void Player::physics(float grav_x, float grav_y)
 	{
 		setY(SCREEN_HEIGHT - size);
 		velocity.y = velocity.y * bounciness;
-		if(velocity.x >= -1 && velocity.x <= 1)
+		if(velocity.x >= -0.5 && velocity.x <= 0.5)
 		{
 			velocity.x = 0;
 		}
@@ -93,7 +93,7 @@ void Player::physics(float grav_x, float grav_y)
 	{
 		setY(0);
 		velocity.y = -velocity.y * -bounciness;
-		if(velocity.x >= -1 && velocity.x <= 1)
+		if(velocity.x >= -0.5 && velocity.x <= 0.5)
 		{
 			velocity.x = 0;
 		}
@@ -111,7 +111,7 @@ void Player::physics(float grav_x, float grav_y)
 	{
 		setX(SCREEN_WIDTH - size);
 		velocity.x = velocity.x * bounciness;
-		if(velocity.y >= -1 && velocity.y <= 1)
+		if(velocity.y >= -0.5 && velocity.y <= 0.5)
 		{
 			velocity.y = 0;
 		}
@@ -128,7 +128,7 @@ void Player::physics(float grav_x, float grav_y)
 	{
 		setX(0);
 		velocity.x = -velocity.x * -bounciness;
-		if(velocity.y >= -1 && velocity.y <= 1)
+		if(velocity.y >= -0.5 && velocity.y <= 0.5)
 		{
 			velocity.y = 0;
 		}
